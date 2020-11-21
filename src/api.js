5430034efe4908c570e93ff36dbb716959e4096a
Surefirer <46170182+Surefirer@users.noreply.github.com>
2020-11-21 13:23:07 -0800

    setup redux

--- src/api.js
+++ src/api.js
@@ -31,3 +31,5 @@ const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
 // Popular Games
 const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+export const popularGamesURL = () => `${base_url}${popular_games}`;
